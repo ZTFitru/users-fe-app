a732c3553aa62913b4bac2de058316d28c6937b0
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-10-12 19:25:14 -0400

    Created a host configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'user-host',
+      remotes: {
+        // todo_components: "http://localhost:4173/assets/remoteEntry.js",
+        chess_fe_microservice: "http://localhost:4173/assets/remoteEntry.js",
+      },
+      shared: ["react"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
+
+
+
