7ab276865fc22db260d6dc79e836b45666ac16c7
Seth Way <way.sethaniel@gmail.com>
2024-10-26 14:06:44 -0400

    add tests for show friends / remove frind

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  viewportWidth: 414,
+  viewportHeight: 736,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
